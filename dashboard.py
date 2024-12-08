27d03b38c83c118665ad6e32ea623b3ccad77c57
Oskar Allerslev <oskar.m1660@gmail.com>
2024-12-08 21:14:56 +0100

    added more info to RS

--- dashboard.py
+++ dashboard.py
@@ -26,7 +26,7 @@ data_client = StockHistoricalDataClient(API_KEY, API_SECRET)
 symbol_spy = "SPY"
 symbol_trade = "SPXL"
 
-config_path = r"C:\Users\Oskar\OneDrive\strategytrader\trader\config\config.yaml.txt"
+config_path = r"\config\config.yaml.txt"
 
 with open(config_path, "r") as file:
     config = yaml.safe_load(file)
